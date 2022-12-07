10cd9045656cb458f1227a7bf21874684c51b7c8
Marina <z.marina3@gmail.com>
2022-12-07 00:10:14 -0600

    first commit

---
+++ example1.js
@@ -0,0 +1,14 @@
+let a = "Hello";
+let b = 12;
+let c = 3.14*2;
+let d = b+c;
+
+const pi_number = 3.14;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(pi_number);
+console.log("The result is = " + (b+c+d));
+
---
+++ example2.js
@@ -0,0 +1,9 @@
+console.log(multTwoNum(2,5));
+console.log(multTwoNum(7,5));
+console.log(multTwoNum(8, 2.15));
+console.log(multTwoNum(2,5));
+
+function multTwoNum(numOne, numTwo){
+    let result = numOne*numTwo;
+    return result;
+}
